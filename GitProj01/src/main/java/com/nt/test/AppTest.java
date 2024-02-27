89d16a7d09cc2a5aec30f5c12842c13b083f83f6
Jayasree <ujayasree1999@gmail.com>
2024-02-27 18:53:52 +0530

    TL -- Task1

--- GitProj01/src/main/java/com/nt/test/AppTest.java
+++ GitProj01/src/main/java/com/nt/test/AppTest.java
@@ -6,7 +6,7 @@ public class AppTest {
 		ArithmeticOperations ar = new ArithmeticOperations();
 		System.out.println(ar.sum(100, 200));
 		System.out.println(ar.sub(200, 100));
-
+		System.out.println("----------------");
 	}
 
 }
